0f3a88be8ed36c7c3d1792e271df8606c9084bd0
Jack Huỳnh <trunghieu.huynh@emma-sleep.com>
2026-09-26 16:41:09 +0700

    feat(cli): add dg clickup create-folder command

--- packages/cli/src/clickup/index.ts
+++ packages/cli/src/clickup/index.ts
@@ -5,6 +5,7 @@ import { getTaskCommand } from "./get-task.ts";
 import { getListsCommand } from "./get-lists.ts";
 import { createTaskCommand } from "./create-task.ts";
 import { updateTaskCommand } from "./update-task.ts";
+import { createFolderCommand } from "./create-folder.ts";
 import { uploadAttachmentCommand } from "./upload-attachment.ts";
 
 const clickupCommand: CommandModule = {
@@ -18,6 +19,7 @@ const clickupCommand: CommandModule = {
       .command(getListsCommand)
       .command(createTaskCommand)
       .command(updateTaskCommand)
+      .command(createFolderCommand)
       .command(uploadAttachmentCommand)
       .demandCommand(1, ""),
   handler: () => {},
